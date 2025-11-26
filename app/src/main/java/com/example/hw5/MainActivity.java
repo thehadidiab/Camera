53e842888546c262ed262daad9f02dbd844ba50b
PC-04 <ahmed.abu.21418@gmail.com>
2025-11-26 12:20:45 +0200

    wow

---
+++ app/src/main/java/com/example/hw5/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hw5;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button btn;
+    private ImageView iv;
+    private Bitmap bm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.picbtn);
+        iv= findViewById(R.id.imgvw);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0){
+            if(resultCode==RESULT_OK){
+                bm=(Bitmap)data.getExtras().get("data");
+                iv.setImageBitmap(bm);
+            }
+        }
+    }
+}
